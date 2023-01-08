841838a87eee4d76e43714461dd55a2fc01f9139
shinbm44 <78016592+shinbm44@users.noreply.github.com>
2023-01-08 17:24:41 +0900

    Update one.java

--- one.java
+++ one.java
@@ -8,16 +8,17 @@ public class one {
 		
 		
 		
-		for( int i = 1; i<10; i++) {
-			for( int j =1; j<10; j++) {
-				int num = i *j;
+	for( int i = 1; i<10; i++) {
+		for( int j =1; j<10; j++) {
+			int num = i *j;
 				
-				System.out.print( String.format("%d", i)
-									+ String.format("%2s", "x")
-									+ String.format("%2d", j)
-									+ String.format("%2s", "=")
-									+ String.format("%2d", num)
-									+ String.format("%s", "\t")
+			System.out.print( String.format("%d", i)
+				+ String.format("%2s", "x")
+				+ String.format("%2d", j)
+				+ String.format("%2s", "=")
+				+ String.format("%2d", num)
+				+ String.format("%s", "\t")
+	
 														);	
 			}
 			System.out.println();
